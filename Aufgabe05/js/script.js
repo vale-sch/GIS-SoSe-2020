59503cb228bb10603ba6945904c73efa470abc67
Valentin Schmidberger <vali.vogt@gmx.de>
2020-05-27 11:59:03 +0200

    Anfang Aufgabe 05

--- Aufgabe05/js/script.js
+++ Aufgabe05/js/script.js
@@ -1,3 +1,6 @@
 "use strict";
-console.log("HAllo Welt");
+const main = document.getElementById("main");
+const p = document.createElement("p");
+p.textContent = "Hello, World!";
+main?.appendChild(p);
 //# sourceMappingURL=script.js.map
--- Aufgabe05/js/script.ts
+++ Aufgabe05/js/script.ts
@@ -1,1 +1,4 @@
-console.log("HAllo Welt");
+const main = document.getElementById("main");
+const p = document.createElement("p");
+p.textContent = "Hello, World!";
+main?.appendChild(p);
