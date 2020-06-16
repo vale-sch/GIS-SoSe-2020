e2355fb98b8ab875c3f5a8b2ce664b3d94192b31
Vale <vali.vogt@gmx.de>
2020-06-16 21:05:12 +0200

    Start Aufgabe08

--- Aufgabe08/test.js
+++ Aufgabe08/test.js
@@ -1,21 +1,19 @@
 "use strict";
-/*import * as Http from "http";
-import * as Url from "url";
-
-export namespace aufgabe08 {
-    
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.aufgabe08 = void 0;
+const Http = require("http");
+var aufgabe08;
+(function (aufgabe08) {
     test08();
-    Http.createServer();
-    async function test08(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://whatever.server/path/file";
+    async function test08() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://compaktdisk.herokuapp.com";
         // tslint:disable-next-line: no-any
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        let query = new URLSearchParams(formData);
         url += url + "?" + query.toString();
         await fetch(url);
     }
-
-
-}*/
+    Http.createServer();
+})(aufgabe08 = exports.aufgabe08 || (exports.aufgabe08 = {}));
 console.log("test");
 //# sourceMappingURL=test.js.map
--- Aufgabe08/test.ts
+++ Aufgabe08/test.ts
@@ -1,20 +1,18 @@
-/*import * as Http from "http";
-import * as Url from "url";
-
+import * as Http from "http";
 export namespace aufgabe08 {
     
     test08();
-    Http.createServer();
+   
     async function test08(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://whatever.server/path/file";
+        let url: string = "https://compaktdisk.herokuapp.com";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += url + "?" + query.toString();
         await fetch(url);
     }
 
-
-}*/
+    Http.createServer();
+}
 
 console.log("test");
