17a2e973a78f909dc59d364d6f676b1369e7bfe2
Vale <vali.vogt@gmx.de>
2020-06-16 21:32:18 +0200

    last try fot today

--- Aufgabe08/test.ts
+++ Aufgabe08/test.ts
@@ -1,19 +1,19 @@
 import * as Http from "http";
 import * as Url from "url";
-export namespace aufgabe08 {
+/*export namespace aufgabe08 {
     
     test08();
    
     async function test08(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://compaktdisk.herokuapp.com/";
+        let url: string = "formular.htm";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += url + "?" + query.toString();
         await fetch(url);
     }
 
-    Http.createServer();
-}
-
+   
+}*/
+Http.createServer();
 console.log("test");
