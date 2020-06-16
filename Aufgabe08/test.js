3f67aee84a794d28f4b6cf4f149e115b12240c8a
Vale <vali.vogt@gmx.de>
2020-06-16 20:52:46 +0200

    das war ja ein krampf he

---
+++ Aufgabe08/test.js
@@ -0,0 +1,21 @@
+"use strict";
+/*import * as Http from "http";
+import * as Url from "url";
+
+export namespace aufgabe08 {
+    
+    test08();
+    Http.createServer();
+    async function test08(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://whatever.server/path/file";
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += url + "?" + query.toString();
+        await fetch(url);
+    }
+
+
+}*/
+console.log("test");
+//# sourceMappingURL=test.js.map
