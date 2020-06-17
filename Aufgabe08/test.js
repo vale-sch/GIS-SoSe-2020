ec456bf939f31f07a09f220e489575f4c9c2c259
Vale <vali.vogt@gmx.de>
2020-06-17 10:10:17 +0200

    try

--- Aufgabe08/test.js
+++ Aufgabe08/test.js
@@ -1,19 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.aufgabe08 = void 0;
+exports.A08Server = void 0;
 const Http = require("http");
-var aufgabe08;
-(function (aufgabe08) {
-    test08();
-    async function test08() {
-        let formData = new FormData(document.forms[0]);
-        let url = "https://compaktdisk.herokuapp.com/";
-        // tslint:disable-next-line: no-any
-        let query = new URLSearchParams(formData);
-        url += url + "?" + query.toString();
-        await fetch(url);
+var A08Server;
+(function (A08Server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
     }
-    Http.createServer();
-})(aufgabe08 = exports.aufgabe08 || (exports.aufgabe08 = {}));
-console.log("test");
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
+        _response.end();
+    }
+})(A08Server = exports.A08Server || (exports.A08Server = {}));
 //# sourceMappingURL=test.js.map
--- Aufgabe08/test.ts
+++ Aufgabe08/test.ts
@@ -1,19 +1,28 @@
 import * as Http from "http";
-import * as Url from "url";
-/*export namespace aufgabe08 {
-    
-    test08();
-   
-    async function test08(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "formular.htm";
-        // tslint:disable-next-line: no-any
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url += url + "?" + query.toString();
-        await fetch(url);
-    }
 
-   
-}*/
-Http.createServer();
-console.log("test");
+export namespace A08Server {
+  console.log("Starting server");
+  let port: number = Number(process.env.PORT);
+  if (!port)
+    port = 8100;
+
+  let server: Http.Server = Http.createServer();
+  server.addListener("request", handleRequest);
+  server.addListener("listening", handleListen);
+  server.listen(port);
+
+  function handleListen(): void {
+    console.log("Listening");
+  }
+
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("I hear voices!");
+
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+
+    _response.write(_request.url);
+
+    _response.end();
+  }
+}
