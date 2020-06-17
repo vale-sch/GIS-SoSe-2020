4d05e26c78024dfffad5c16dc9ef298d5e0fc6a2
Vale <vali.vogt@gmx.de>
2020-06-17 11:33:21 +0200

    teilaufgabe02

--- Aufgabe08/test.ts
+++ Aufgabe08/test.ts
@@ -14,10 +14,18 @@ export namespace A08Server {
   function handleListen(): void {
     console.log("Listening");
   }
+  async function fetchURl(): Promise <void> {
+    let formData: FormData = new FormData(document.forms[0]);
+    let url: string = "https://compaktdisk.herokuapp.com/";
+    // tslint:disable-next-line: no-any
+    let query: URLSearchParams = new URLSearchParams(<any>formData);
+    url += url + "?" + query.toString();
+    await fetch(url);
+  }
 
   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
     console.log("I hear voices!");
-
+    fetchURl();
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
 
