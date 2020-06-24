c088dafbd9e5fc1e4531a2f9420be93eea40cf33
Vale <vali.vogt@gmx.de>
2020-06-24 15:14:05 +0200

    changes

--- Aufgabe09/testClass.js
+++ Aufgabe09/testClass.js
@@ -2,7 +2,8 @@
 var A09Server;
 (function (A09Server) {
     window.addEventListener("load", init);
-    let navLi;
+    let navLi0;
+    let navLi1;
     let div;
     let text;
     let responseJson;
@@ -10,26 +11,33 @@ var A09Server;
         div = document.querySelector("h1");
         text = document.createElement("p");
         //Der Button mit der id hallo wird in dem Element navLi eingespeichert
-        navLi = document.querySelector("#hallo");
+        navLi0 = document.querySelector("#button0");
+        navLi1 = document.querySelector("#button1");
         //onClickButton wird bei einem Click auf den Button ausgeführt
-        navLi.addEventListener("click", onClickButton.bind(navLi));
+        navLi0.addEventListener("click", onClickButton0.bind(navLi0));
+        navLi1.addEventListener("click", onClickButton1.bind(navLi1));
     }
-    async function onClickButton(_click) {
+    async function onClickButton0(_click) {
+        let url = "https://compaktdisk.herokuapp.com/";
+        let formData = new FormData(document.forms[0]);
+        // tslint:disable-next-line: no-any
+        let query = new URLSearchParams(formData);
+        url += "?" + query.toString();
+        let response = await fetch(url);
+        let responseText = await response.text();
+        div.appendChild(text).innerHTML = "Eingabe: " + responseText;
+    }
+    async function onClickButton1(_click) {
         let url = "https://compaktdisk.herokuapp.com/";
         let formData = new FormData(document.forms[0]);
         //Form-Element aus der HTML Datei ziehen
-        //let url: string = "https://compaktdisk.herokuapp.com/";
-        // tslint:disable-next-line: no-any
-        //alle Zeichen nach dem "?"
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
         url += "?" + query.toString();
         let response = await fetch(url);
         let responseText = await response.text();
         responseJson = JSON.parse(responseText);
-        console.log(responseText);
         console.log(responseJson);
-        div.appendChild(text).innerHTML = "Eingabe: " + responseText;
     }
 })(A09Server || (A09Server = {}));
 //# sourceMappingURL=testClass.js.map
--- Aufgabe09/testClass.ts
+++ Aufgabe09/testClass.ts
@@ -1,6 +1,7 @@
 namespace A09Server {
   window.addEventListener("load", init);
-  let navLi: HTMLButtonElement;
+  let navLi0: HTMLButtonElement;
+  let navLi1: HTMLButtonElement;
   let div: HTMLDivElement;
   let text: HTMLElement;
   let responseJson: JSON;
@@ -9,21 +10,37 @@ namespace A09Server {
     div = <HTMLDivElement>document.querySelector("h1");
     text = document.createElement("p");
     //Der Button mit der id hallo wird in dem Element navLi eingespeichert
-    navLi = <HTMLButtonElement>document.querySelector("#hallo");
+    navLi0 = <HTMLButtonElement>document.querySelector("#button0");
+    navLi1 = <HTMLButtonElement>document.querySelector("#button1");
     //onClickButton wird bei einem Click auf den Button ausgeführt
-    navLi.addEventListener("click", onClickButton.bind(navLi));
+    
+    navLi0.addEventListener("click", onClickButton0.bind(navLi0));
+    navLi1.addEventListener("click", onClickButton1.bind(navLi1));
 
     
   }
 
-  async function onClickButton(_click: MouseEvent): Promise<void> {
+  async function onClickButton0(_click: MouseEvent): Promise<void> {
+    let url: string = "https://compaktdisk.herokuapp.com/";
+    let formData: FormData = new FormData(document.forms[0]);
+    
+    // tslint:disable-next-line: no-any
+    let query: URLSearchParams = new URLSearchParams(<any>formData);
+
+    url += "?" + query.toString();
+    let response: Response = await fetch(url);
+
+    let responseText: string = await response.text();
+
+    div.appendChild(text).innerHTML = "Eingabe: " + responseText;
+
+  }
+  async function onClickButton1(_click: MouseEvent): Promise<void> {
     let url: string = "https://compaktdisk.herokuapp.com/";
     let formData: FormData = new FormData(document.forms[0]);
     //Form-Element aus der HTML Datei ziehen
 
-    //let url: string = "https://compaktdisk.herokuapp.com/";
-    // tslint:disable-next-line: no-any
-    //alle Zeichen nach dem "?"
+    
     // tslint:disable-next-line: no-any
     let query: URLSearchParams = new URLSearchParams(<any>formData);
 
@@ -33,13 +50,10 @@ namespace A09Server {
     let responseText: string = await response.text();
 
     responseJson = JSON.parse(responseText);
-
-    console.log(responseText);
     console.log(responseJson);
-    
 
-    div.appendChild(text).innerHTML = "Eingabe: " + responseText;
 
   }
+
 
 }
